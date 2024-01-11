f1aee9bf723b9f71faa71549fb6e628e097275d8
ChaseRensberger <chasehrensberger@gmail.com>
2024-01-11 17:36:23 -0500

    crate basic table

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,10 +1,9 @@
 "use client";
-import TransactionsTable from "@/components/transactions/transactionsTable";
+import TransactionTable from "@/components/transactions/transactionsTable";
 export default function Transactions() {
   return (
     <>
-      <h1>Welcome to Transactions</h1>
-      <TransactionsTable />
+      <TransactionTable />
     </>
   );
 }
--- src/components/transactions/transactionsTable.tsx
+++ src/components/transactions/transactionsTable.tsx
@@ -1,42 +1,98 @@
 "use client";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { useState } from "react";
 
-export default function TransactionsTable() {
-  const account1 = {
-    name: "PNC Checking",
-    type: "Checking",
-    balance: 12234,
-  };
-  const [accounts, setAccounts] = useState([account1]);
+import { ColumnDef } from "@tanstack/react-table";
+import { DataTable } from "../shared/dataTable";
+import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export const transactions: Transaction[] = [
+  {
+    id: "string",
+    account_from: "PNC Checking",
+    account_to: "McDonalds",
+    amount: 100,
+    time: new Date(),
+  },
+];
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Transaction = {
+  id: string;
+  time: Date;
+  account_from: string;
+  account_to: string;
+  amount: number;
+};
+
+export const columns: ColumnDef<Transaction>[] = [
+  {
+    accessorKey: "time",
+    header: "Date",
+  },
+  {
+    accessorKey: "account_from",
+    header: "Account From",
+  },
+  {
+    accessorKey: "account_to",
+    header: "Account To",
+  },
+  {
+    accessorKey: "amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount);
+
+      return <span className="text-right font-medium">{formatted}</span>;
+    },
+  },
+  // {
+  //   id: "actions",
+  //   cell: ({ row }) => {
+  //     const transaction = row.original;
+
+  //     return (
+  //       <DropdownMenu>
+  //         <DropdownMenuTrigger asChild>
+  //           <Button variant="ghost" className="h-8 w-8 p-0">
+  //             <span className="sr-only">Open menu</span>
+  //             <EllipsisHorizontalIcon className="h-4 w-4" />
+  //           </Button>
+  //         </DropdownMenuTrigger>
+  //         <DropdownMenuContent align="end">
+  //           <DropdownMenuLabel>Actions</DropdownMenuLabel>
+  //           <DropdownMenuItem
+  //             onClick={() => navigator.clipboard.writeText(transaction.id)}
+  //           >
+  //             Copy payment ID
+  //           </DropdownMenuItem>
+  //           <DropdownMenuSeparator />
+  //           <DropdownMenuItem>View customer</DropdownMenuItem>
+  //           <DropdownMenuItem>View payment details</DropdownMenuItem>
+  //         </DropdownMenuContent>
+  //       </DropdownMenu>
+  //     );
+  //   },
+  // },
+];
+
+export default async function TransactionTable() {
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Date</TableHead>
-          <TableHead>From</TableHead>
-          <TableHead>To</TableHead>
-          <TableHead>Amount</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {accounts.map((account) => {
-          return (
-            <TableRow key={account.name} className="hover:cursor-pointer">
-              <TableCell>{account.name}</TableCell>
-              <TableCell>{account.type}</TableCell>
-              <TableCell>${account.balance}</TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+    <div>
+      <DataTable columns={columns} data={transactions} />
+    </div>
   );
 }
