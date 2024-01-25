6d5253e5824ac2e4e6ff70e226474f65c8738fdc
ChaseRensberger <chasehrensberger@gmail.com>
2024-01-24 19:47:23 -0500

    add HttpRequest helper function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export async function HttpRequest(
+  url: string,
+  method: string,
+  data: Object | null = null
+) {
+  const res = await fetch(url, {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return res.json();
+}
