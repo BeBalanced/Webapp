5c7b18a3c745aaf6535f21b35a7937d47ef542a1
ChaseRensberger <chasehrensberger@gmail.com>
2024-01-29 22:45:52 -0500

    remove console log

--- src/app/api/plaid/accounts/route.ts
+++ src/app/api/plaid/accounts/route.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { plaid_client } from "@/lib/plaid/config";
 
 export async function GET(request: NextRequest) {
-  console.log("hey");
   const url = new URL(request.url);
   const access_token: string = url.searchParams.get("access_token")!;
   try {
